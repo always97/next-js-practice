f3a3e6c196115f7d2f44996e7f96ed21ccfa0007
jw <always1997@kakao.com>
2022-08-21 23:55:38 +0900

    detail page title pre-render serverSide

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 
 import { useEffect,useState } from 'react';
 import Seo from '../components/Seo';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const API_KEY = '';
 
 export default function Home({results}) {
   
+  const router = useRouter();
+  const handleClick = (id,title) => {
+    router.push(`/movies/${title}/${id}`)
+  }
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div onClick={()=>handleClick(movie.id,movie.original_title)} className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+            <a>{movie.original_title}</a>
+          </Link>
         </div>
       ))}
 
---
+++ pages/movies/[...params].js
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import Seo from '../../components/Seo';
+
+const Detail = ({params}) => {
+  const router = useRouter();
+  const  [title, id] = params || [];
+  return (
+    <div>
+      <Seo title={title} />
+      <h4>{title}</h4>
+    </div>
+  );
+};
+
+export default Detail;
+
+export function getServerSideProps({params:{params}}) {
+
+  return {
+    props: {
+      params,
+    }
+  }
+}
